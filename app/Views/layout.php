0c68084eeea63878a1127e02534fb2ea2a81e067
E.Cannemeijer <e.cannemeijer@hotmail.com>
2026-02-20 15:39:53 +0100

    remove: verwijder grafieken en breakdowns van dashboard

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -513,8 +513,7 @@
     <!-- Bootstrap 5 JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     
-    <!-- Chart.js -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
+
     
     <?= $this->renderSection('scripts') ?>
 </body>
